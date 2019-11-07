1fef9821c8de54a0444001e500a243f7979d533f
Justin Kane <JustinKane197@gmail.com>
2019-11-07 13:48:53 -0500

    reached MVP

--- application-testing/src/App.js
+++ application-testing/src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
+import Display from "./components/Display";
+import Buttons from "./components/Buttons";
+
 function App() {
+
+  const [balls, setBalls] = useState(0);
+  const [strikes, setStrikes] = useState(0);
+
   return (
-    <div>
-      
+    <div className="App">
+      <header data-testid="main-header" className="App-header">
+        <p>
+          Web-Application-Testing
+        </p>
+
+        <Display balls={balls} strikes={strikes}/>
+        <Buttons balls={balls} strikes={strikes} setBalls={setBalls} setStrikes={setStrikes} />
+
+      </header>
     </div>
   );
 }
